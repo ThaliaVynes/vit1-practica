1601d8e9970834973857aebc8239fef9ede5a080
thaliavynes1 <casamarketthalia@email.com>
2026-05-05 16:33:34 -0500

    ahorasi

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,17 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [tailwindcss()],
+
+  build: {
+    rollupOptions: {
+      input: {
+        Inicio: path.resolve(__dirname, 'index.html'),
+        Informacion: path.resolve(__dirname, 'index2.html'),
+        Final: path.resolve(__dirname, 'index3.html'),
+      },
+    },
+  },
 })
